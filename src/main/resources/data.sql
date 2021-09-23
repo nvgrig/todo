ed8ff3c4ecb39295e9b58b2e0437296bc5a28f97
Nikita Grigoryev <nvgrigoryev@gmail.com>
2021-09-23 21:32:55 +0400

    application.yaml setup & test db init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (login, password)
+VALUES ('user', 'password');
+
+INSERT INTO todo (description, is_completed, creation_date, user_id)
+VALUES ('buy milk', 'false', '2021-09-23', '1'),
+       ('call mom', 'false', '2021-09-23', '1');
