e30f9dd2a06821ab7ec0390602da4afe0f8255dc
Nikita Grigoryev <nvgrigoryev@gmail.com>
2021-09-25 14:44:57 +0400

    UserService implementation

--- src/main/java/com/github/nvgrig/todo/repository/UserRepository.java
+++ src/main/java/com/github/nvgrig/todo/repository/UserRepository.java
@@ -1,7 +1,17 @@
 package com.github.nvgrig.todo.repository;
 
 import com.github.nvgrig.todo.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("DELETE FROM User u WHERE u.id=:id")
+    int delete(@Param("id") int id);
+
+    @EntityGraph(attributePaths = {"todoList"}, type = EntityGraph.EntityGraphType.LOAD)
+    @Query("SELECT u FROM User u WHERE u.id=:id")
+    User getWithTodos(@Param("id") int id);
 }
--- src/main/java/com/github/nvgrig/todo/service/UserService.java
+++ src/main/java/com/github/nvgrig/todo/service/UserService.java
@@ -2,8 +2,12 @@ package com.github.nvgrig.todo.service;
 
 import com.github.nvgrig.todo.model.User;
 import com.github.nvgrig.todo.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
+@Service
 public class UserService {
+
     private final UserRepository repository;
 
     public UserService(UserRepository repository) {
@@ -11,18 +15,20 @@ public class UserService {
     }
 
     public User create(User user) {
-        return null;
+        Assert.notNull(user, "user must not be null");
+        return repository.save(user);
     }
 
     public void delete(int id) {
+        repository.delete(id);
     }
 
     public User get(int id) {
-        return null;
+        return repository.findById(id).orElse(null);
     }
 
     public User getWithTodos(int id) {
-        return null;
+        return repository.getWithTodos(id);
     }
 
 }
